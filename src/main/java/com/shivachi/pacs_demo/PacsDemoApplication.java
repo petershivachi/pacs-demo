18c0523f69e10c19e1e8ebac77d1e5b61062a026
petershivachi <shitsukanepeter@gmail.com>
2024-07-23 11:01:51 +0300

    initial commit

---
+++ src/main/java/com/shivachi/pacs_demo/PacsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shivachi.pacs_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivachi/pacs_demo/PacsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivachi.pacs_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
